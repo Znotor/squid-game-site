653dde16de472763f350676f4e46461b61dba0ec
Znotor <155180477+Znotor@users.noreply.github.com>
2024-07-21 00:24:43 +0200

    Update Cell.js

--- Cell.js
+++ Cell.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import './Cell.css';
 
+const Cell = ({ isActive, participant }) => {
+  return (
+    <div className={`cell ${isActive ? 'active' : ''}`}>
+      {participant.number && (
+        <>
+          <img 
+            className="photo" 
+            src={`https://picsum.photos/200?random=${participant.number}`} 
+            alt={participant.name} 
+          />
+          <div className="number">{participant.number}</div>
+          <div className="name">{participant.name}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Cell;
